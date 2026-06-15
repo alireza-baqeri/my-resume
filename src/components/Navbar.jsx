d9cef28ca9e52f3f3276ad9e8d72bb0abba871f6
Alireza Bagheri <abrahamgoldman13@gmail.com>
2026-06-15 03:33:49 +0330

    Adding & Fixing Logo-Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,161 @@
-import { useState } from "react";
-function Navbar() {
+// import { useState } from "react";
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
-    return (<>
-        <nav>
-            <div className={"container flex items-center justify-between gap-10 "}>
-                <div className={"logo"}>
-                    <span className={"border rounded shadow-xl shadow-brand-yellow/75"}>LOGO</span>
-                </div>
+const navigation = [
+    { name: 'About', href: '#', current: true },
+    { name: 'Skills', href: '#', current: false },
+    { name: 'Projects', href: '#', current: false },
+    { name: 'Contact', href: '#', current: false },
+]
 
-                <ul className="desktop-menu grid grid-cols-4 gap-4 mt-3 ">
-                    <li className="border rounded-sm p-2 text-center">About</li>
-                    <li className="border rounded-sm p-2 text-center">Skills</li>
-                    <li className="border rounded-sm p-2 text-center">Projects</li>
-                    <li className="border rounded-sm p-2 text-center">Contact</li>
-                </ul>
 
-                <div className={"actions"}>
-                    Theme
-                    Hamburger
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+
+
+
+export default function Navbar() {
+
+    return (
+        <Disclosure
+            as="nav"
+            className="relative bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10"
+        >
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div className="relative flex h-16 items-center justify-between">
+                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                        {/* Mobile menu button*/}
+                        <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+                            <span className="absolute -inset-0.5" />
+                            <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
+                            <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+                        </DisclosureButton>
+                    </div>
+
+                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="flex shrink-0 items-center">
+                            <img
+                                alt="Your Company"
+                                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                                className="h-8 w-auto"
+                            />
+                        </div>
+                        <div className="hidden sm:ml-6 sm:block">
+                            <div className="flex space-x-4">
+                                {navigation.map((item) => (
+                                    <a
+                                        key={item.name}
+                                        href={item.href}
+                                        aria-current={item.current ? 'page' : undefined}
+                                        className={classNames(
+                                            item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                                            'rounded-md px-3 py-2 text-sm font-medium',
+                                        )}
+                                    >
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                        <button
+                            type="button"
+                            className="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
+                        >
+                            <span className="absolute -inset-1.5" />
+                            <span className="sr-only">View notifications</span>
+                            <BellIcon aria-hidden="true" className="size-6" />
+                        </button>
+
+                        {/* Profile dropdown */}
+                        <Menu as="div" className="relative ml-3">
+                            <MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                                <span className="absolute -inset-1.5" />
+                                <span className="sr-only">Open user menu</span>
+                                <img
+                                    alt=""
+                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                    className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10"
+                                />
+                            </MenuButton>
+
+                            <MenuItems
+                                transition
+                                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-gray-800 py-1 outline -outline-offset-1 outline-white/10 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                            >
+                                <MenuItem>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:outline-hidden"
+                                    >
+                                        Your profile
+                                    </a>
+                                </MenuItem>
+                                <MenuItem>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:outline-hidden"
+                                    >
+                                        Settings
+                                    </a>
+                                </MenuItem>
+                                <MenuItem>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:outline-hidden"
+                                    >
+                                        Sign out
+                                    </a>
+                                </MenuItem>
+                            </MenuItems>
+                        </Menu>
+                    </div>
                 </div>
             </div>
-        </nav>
 
-        </>
-        );
+            <DisclosurePanel className="sm:hidden">
+                <div className="space-y-1 px-2 pt-2 pb-3">
+                    {navigation.map((item) => (
+                        <DisclosureButton
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            aria-current={item.current ? 'page' : undefined}
+                            className={classNames(
+                                item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                                'block rounded-md px-3 py-2 text-base font-medium',
+                            )}
+                        >
+                            {item.name}
+                        </DisclosureButton>
+                    ))}
+                </div>
+            </DisclosurePanel>
+        </Disclosure>
+    );
         }
 
-        export default Navbar;
 
+
+// <nav className={"border-b pb-5 "}>
+//     <div className={"container flex items-center justify-between gap-10 pt-3"}>
+//         <div className={"logo"}>
+//             <span className={"border rounded shadow-xl shadow-brand-yellow/75"}>LOGO</span>
+//         </div>
+//
+//         {/*<ul className="desktop-menu grid grid-cols-4 gap-4 ">*/}
+//         {/*    <li className="border rounded-sm p-2 text-center">About</li>*/}
+//         {/*    <li className="border rounded-sm p-2 text-center">Skills</li>*/}
+//         {/*    <li className="border rounded-sm p-2 text-center">Projects</li>*/}
+//         {/*    <li className="border rounded-sm p-2 text-center">Contact</li>*/}
+//         {/*</ul>*/}
+//
+//     </div>
+// </nav>
 
 
 
