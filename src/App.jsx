3c579418fe25653c1f790aa5a5c014d549274969
Alireza Bagheri <abrahamgoldman13@gmail.com>
2026-06-17 11:34:08 +0330

    Experience component is now added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
@@ -12,6 +13,7 @@ function App() {
         <Hero />
         <About />
         <Skills />
+          <Experience/>
         <Projects
             title="Shop App"
             description="React + Tailwind"
